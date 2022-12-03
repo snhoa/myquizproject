1581e5c0dc47ddc807b074e1f74a2b6d59b8247b
snhoa <snhoa16@naver.com>
2022-12-03 18:53:35 +0900

    commit message

--- EIP_Master/settings.py
+++ EIP_Master/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ.get['DJANGO_SECRET_KEY', 'django-insecure-tv72iz3cyr(@x=gfszfbg)qq*%*!hme%yhw_clpdi3efy)(%-&']
 
-#'django-insecure-tv72iz3cyr(@x=gfszfbg)qq*%*!hme%yhw_clpdi3efy)(%-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
